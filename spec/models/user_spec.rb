3f044b0b0bfb62e5ebe04eba897160da074e55b0
haynescw <haynescw@uwec.edu>
2013-03-08 12:03:46 -0600

    Finish user edit, update, index, adn destroy actions

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,6 +13,21 @@ require 'spec_helper'
 
 describe User do
   
+  it { should respond_to(:admin) }
+  it { should respond_to(:authenticate) }
+
+  it { should be_valid }
+  it { should_not be_admin }
+
+  describe "with admin attributes set to 'true'" do
+    before do
+      @user.save!
+      @user.toggle!(:admin)
+    end
+
+    it { should be_admin }
+  end
+
   before do
     @user = User.new(:name=> "Example User", :email=> "user@example.com",     
                      :password => "foobar", :password_confirmation => "foobar")
